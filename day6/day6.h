d320c27de6edcd0d69588f6b0b6a1fa2422e5cfb
abhinavmk00 <abhinavmk00@gmail.com>
2024-07-27 09:11:28 +0530

    day 4-5 added

---
+++ day6/day6.h
@@ -0,0 +1,10 @@
+//Write a program to store and print the roll no., name, age and marks of 10 students using structures.
+
+struct student{
+int rno;
+char name[20];
+int age;
+int marks;
+}
+
+
---
+++ day6/main.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#include "day6.h"
